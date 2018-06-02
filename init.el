702d672fc5f283297d51198688964848edf33610
hare1039 <hare39393@gmail.com>
2018-06-02 19:40:28 +0800

    Update before save hook

--- init.el
+++ init.el
@@ -148,13 +148,13 @@
   (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
+  (add-hook 'before-save-hook 'tide-format-before-save)
   (tide-hl-identifier-mode +1))
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
 ;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
